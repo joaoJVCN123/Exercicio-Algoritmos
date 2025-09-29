d22879437771d169041f5f09ffd18937f17dc311
João Victor C. Nicolai <143816290+joaoJVCN123@users.noreply.github.com>
2025-09-28 22:48:59 -0300

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 int main()
 {
-    double N1A1 , N1A2 , N1A3 , N2A1 , N2A2 , N2A3 , N3A1 , N3A2 , N3A3 , MA1 , MA2 , MA3 ;
-    string A1 , A2 , A3 ;
+    double N1A1 , N1A2 , N1A3 , N1A4 , N1A5 , N2A1 , N2A2 , N2A3 , N2A4 , N2A5 , N3A1 , N3A2 , N3A3 , N3A4 , N3A5 , MA1 , MA2 , MA3 , MA4 , MA5;
+    string A1 , A2 , A3 , A4 , A5 ;
     
     cout<<"Escreva o nome do aluno 1:"<<endl;
     cin>>A1;
@@ -45,6 +45,30 @@ int main()
     
     vector<double> Aluno3{N1A3 , N2A3 , N3A3 , MA3};
     
+    cout<<"===========================\n\n"<<"Escreva o nome do aluno 4:"<<endl;
+    cin>>A4;
+    
+    cout<<"\nEscreva uma de cada vez a N1, N2 e N3 do aluno "<<A4<<":"<<endl;
+    cin>>N1A4;
+    cin>>N2A4;
+    cin>>N3A4;
+    
+    MA4= (N1A4 + N2A4 + N3A4) / 3;
+    
+    vector<double> Aluno4{N1A4 , N2A4 , N3A4 , MA4};
+    
+    cout<<"===========================\n\n"<<"Escreva o nome do aluno 5:"<<endl;
+    cin>>A5;
+    
+    cout<<"\nEscreva uma de cada vez a N1, N2 e N3 do aluno "<<A5<<":"<<endl;
+    cin>>N1A5;
+    cin>>N2A5;
+    cin>>N3A5;
+    
+    MA5= (N1A5 + N2A5 + N3A5) / 3;
+    
+    vector<double> Aluno5{N1A5 , N2A5 , N3A5 , MA5};
+    
     cout<<"===============================================\n\n"<<endl;
     cout<<"As notas, média e resultado de aprovação do aluno "<<A1<<" são as seguintes:\n"<<endl;
     cout<<"N1    N2    N3    Média    Resultado"<<endl;
@@ -74,6 +98,7 @@ int main()
     }
     cout<<"   Reprovado"<<endl;
     }
+    
     cout<<"\n=================================\n"<<"As notas, média e resultado de aprovação do aluno "<<A3<<" são as seguintes:"<<endl;
     cout<<"N1    N2    N3    Média    Resultado"<<endl;
     
@@ -89,20 +114,50 @@ int main()
     cout<<"   Reprovado"<<endl;
     }
     
-     if (MA1 > MA2 and MA1 > MA3) {
+    cout<<"===============================================\n\n"<<endl;
+    cout<<"As notas, média e resultado de aprovação do aluno "<<A4<<" são as seguintes:\n"<<endl;
+    cout<<"N1    N2    N3    Média    Resultado"<<endl;
+    
+    if (MA4 >= 7) {
+        for (double num : Aluno4) {
+        cout << num << "     ";
+        }
+        cout<<"   Aprovado"<<endl;
+    } else if (MA4 < 7) {
+        for (double num : Aluno4) {
+        cout << num << "     ";
+    }
+    cout<<"   Reprovado"<<endl;
+    }
+    
+    cout<<"===============================================\n\n"<<endl;
+    cout<<"As notas, média e resultado de aprovação do aluno "<<A5<<" são as seguintes:\n"<<endl;
+    cout<<"N1    N2    N3    Média    Resultado"<<endl;
+    
+    if (MA5 >= 7) {
+        for (double num : Aluno5) {
+        cout << num << "     ";
+        }
+        cout<<"   Aprovado"<<endl;
+    } else if (MA5 < 7) {
+        for (double num : Aluno5) {
+        cout << num << "     ";
+    }
+    cout<<"   Reprovado"<<endl;
+    }
+    
+     if (MA1 > MA2 and MA1 > MA3 and MA1 > MA4 and MA1 > MA5) {
          cout<<"\n\n================================\n"<<"O aluno com a maior média é o aluno: "<<A1<<endl;
-     } else if (MA2 > MA1 and MA2 > MA3) {
+     } else if (MA2 > MA3 and MA2 > MA4 and MA2 > MA5) {
          cout<<"\n\n================================\n"<<"O aluno com a maior média é o aluno: "<<A2<<endl;
-     } else if (MA3 > MA1 and MA3 > MA2) {
+     } else if (MA3 > MA4 and MA3 > MA5) {
          cout<<"\n\n================================\n"<<"O aluno com a maior média é o aluno: "<<A3<<endl;
-     } else if (MA1 == MA2 and MA1 > MA3) {
-         cout<<"\n\n================================\n"<<"Os alunos "<<A1<<" e "<<A2<<" empatam com a maior média"<<endl;
-     } else if (MA1 == MA3 and MA1 > MA2) {
-         cout<<"\n\n================================\n"<<"Os alunos "<<A1<<" e "<<A3<<" empatam com a maior média"<<endl;
-     } else if (MA2 == MA3 and MA2 > MA1) {
-         cout<<"\n\n================================\n"<<"Os alunos "<<A2<<" e "<<A3<<" empatam com a maior média"<<endl;
+     } else if (MA4 > MA5) {
+         cout<<"\n\n================================\n"<<"O aluno com a maior média é o aluno: "<<A4<<endl;
+     } else {
+         cout<<"\n\n================================\n"<<"O aluno com a maior média é o aluno: "<<A5<<endl;
      }
-     
+
      return 0;
     
 }
